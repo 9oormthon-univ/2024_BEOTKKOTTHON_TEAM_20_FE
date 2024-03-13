5e93cc46220ea6b0324d7dc3af123473d1362cb8
MyungJiwoo <1206jiwoo@gmail.com>
2024-03-13 17:27:46 +0900

    [etc] develop 브랜치 추가

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { Container } from "../styles/MainPageStyled";
+
+const MainPage = () => {
+    return <Container></Container>;
+};
+export default MainPage;
--- src/pages/TestPage.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { Container } from "../styles/TestPageStyled";
-
-const TestPage = () => {
-    return <Container></Container>;
-};
-export default TestPage;
