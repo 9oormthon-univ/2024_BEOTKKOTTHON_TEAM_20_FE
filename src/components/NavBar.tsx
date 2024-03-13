4be204448ab3ebd625b5946ad79aa09e02e89cd2
hongsy521 <seoyeong0521@naver.com>
2024-03-14 00:40:39 +0900

    [feat] mainframe 구성중

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,25 @@
 import React from "react";
 import { Container } from "../styles/NavBarStyled";
 import MainLogo from "../image/MainLogo.png";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
     return <Container>
-        <div>
-            <img src={MainLogo} alt="mainlogo"/>
-            <a>스터디 포스팅</a>
-            <a>AI 퀴즈</a>
-            <a>로그인</a>
-        </div>
+        <nav>
+            <div>
+                <img src={MainLogo} alt="mainlogo"/>
+            </div>
+            <div>
+                <NavLink to="">스터디 포스팅</NavLink>
+            </div>
+            <div>
+                <NavLink to="">AI 퀴즈</NavLink>
+            </div>
+            <div>
+                <NavLink to="">로그인</NavLink>
+            </div>
+        
+        </nav>
     </Container>;
 };
 export default NavBar;
