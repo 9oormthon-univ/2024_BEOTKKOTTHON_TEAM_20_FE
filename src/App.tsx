89c7585eb45f5aeddb72e1ced4b7955aee3d52b0
MyungJiwoo <1206jiwoo@gmail.com>
2024-03-15 19:53:44 +0900

    [feat] 관심사 설정 페이지 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MainPage from "./pages/MainPage";
+import InterestPage from "./pages/InterestPage";
 
 const App: React.FC = () => {
     return (
         <Router>
             <Routes>
                 <Route path="/" Component={MainPage} />
+                <Route path="/interest" Component={InterestPage} />
             </Routes>
         </Router>
     );
---
+++ src/pages/InterestPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Container } from "../styles/InterestPageStyled";
+
+const InterestPage = () => {
+    return (
+        <Container>
+            <h1>관심사 설정</h1>
+        </Container>
+    );
+};
+export default InterestPage;
