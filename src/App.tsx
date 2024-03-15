2d51e8c3d37f524757153e1f55bdca1b6b068bd9
hongsy521 <seoyeong0521@naver.com>
2024-03-16 05:35:27 +0900

    [feat] post 작성 page

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,20 @@ import MainPage from "./pages/MainPage";
 import PostBoardPage from "./pages/PostBoardPage";
 import LoginPage from "./pages/LoginPage";
 import OAuth2RedirectHandler from "./pages/OAuth2RedirectHandler";
+import WritePage from "./pages/WritePage";
 
 const App: React.FC = () => {
     return (
         <Router>
             <Routes>
                 <Route path="/" Component={MainPage} />
-
                 <Route path="/PostBoardPage" Component={PostBoardPage} />
                 <Route path="/login" Component={LoginPage} />
                 <Route
                     path="/auth/redirected/kakao"
                     Component={OAuth2RedirectHandler}
                 />
+                <Route path="/WritePosts" Component={WritePage}/>
             </Routes>
         </Router>
     );
