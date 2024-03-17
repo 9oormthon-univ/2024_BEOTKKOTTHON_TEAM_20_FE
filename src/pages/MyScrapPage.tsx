11cfd047d3c55862d1151b8300279b9b8d6cb4c8
MyungJiwoo <1206jiwoo@gmail.com>
2024-03-17 15:46:07 +0900

    [feat] 마이페이지 스크랩 확인 페이지 디자인

--- src/pages/MyScrapPage.tsx
+++ src/pages/MyScrapPage.tsx
@@ -1,12 +1,26 @@
 import React from "react";
 import { Container } from "../styles/MyScrapPageStyled";
 import MyPageNav from "../components/MyPageNav";
+import BasicPagination from "../components/BasicPagination";
+import PostBox from "../components/PostBox";
 
 const MyScrapPage = () => {
     return (
         <>
             <MyPageNav />
-            <Container>{/* 글 컴포넌트 */}</Container>
+            <Container>
+                <div className="contentBox">
+                    <PostBox />
+                    <PostBox />
+                    <PostBox />
+                    <PostBox />
+                    <PostBox />
+                    <PostBox />
+                </div>
+                <div className="paginationBox">
+                    <BasicPagination />
+                </div>
+            </Container>
         </>
     );
 };
