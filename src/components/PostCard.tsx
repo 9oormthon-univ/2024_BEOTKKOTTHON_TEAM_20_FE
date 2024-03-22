74424107b7c1ce4c21fa4e193b0fe1ef4d3c3e0a
MyungJiwoo <1206jiwoo@gmail.com>
2024-03-23 01:20:15 +0900

    [refactor] 스크랩 추가/취소 #39

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Icon } from "../styles/PostCardStyled";
 import TalkIcon from "../image/commentIcon.png";
 import ScrapIcon from "../image/isScrapIcon.png";
 import { MyPostProps } from "../pages/MyPostPage";
 import { categories } from "./category";
+import axios from "axios";
 
 const PostCard: React.FC<MyPostProps> = ({
     postId,
@@ -17,19 +18,68 @@ const PostCard: React.FC<MyPostProps> = ({
 }) => {
     // 스크랩
     const [isScrapped, setIsScrapped] = useState(false);
+    const [scrapData, setScrapData] = useState([]);
     const scrapHandler = () => {
+        putScrap();
         setIsScrapped(!isScrapped); // toggle
     };
     const fillColor = isScrapped
         ? "opacity(0.1) drop-shadow(0 0 0 #F1AF14)"
         : "";
+
+    // 스크랩 리스트 받아오기
+    // const getData = async () => {
+    //     try {
+    //         const response = await axios.get(
+    //             "https://port-0-qtudy-qxz2elttj8wkd.sel5.cloudtype.app/posts/my-post-list",
+    //             {
+    //                 headers: {
+    //                     Authorization:
+    //                         window.localStorage.getItem("accessToken"),
+    //                 },
+    //             }
+    //         );
+    //         console.log(response.data);
+    //         setScrapData(response.data);
+    //         // 만약 받아온 리스트에 postId가 있으면 setIsScrapped(true)
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+    // };
+
+    // 스크랩 요청하기
+    const putScrap = async () => {
+        console.log(postId);
+        try {
+            const response = await axios.put(
+                `https://port-0-qtudy-qxz2elttj8wkd.sel5.cloudtype.app/posts/scrap?postId=${postId}`,
+                {},
+                {
+                    headers: {
+                        Authorization:
+                            window.localStorage.getItem("accessToken"),
+                    },
+                }
+            );
+            console.log(response.data);
+            setIsScrapped(!isScrapped);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        // getData();
+    }, []);
     return (
         <Container>
             <div className="categoryBox">
                 <p className="category">{categories[categoryId].category}</p>
             </div>
             <div className="titleBox">
-                <p className="title">{title}</p>
+                <p className="title">
+                    {title.length > 10 ? title.slice(0, 10) + " ..." : title}
+                </p>
                 <p className="date">{createdAt.slice(0, 10)}</p>
             </div>
             <div className="PostBox">
