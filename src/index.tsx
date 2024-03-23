b0958ae7a63953ec1b0956463ec56d1278985964
MyungJiwoo <1206jiwoo@gmail.com>
2024-03-23 21:30:14 +0900

    [refactor] 배포  #50

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { Desktop } from "../src/pages/mediaQuery";
+import { Desktop } from "./pages/mediaQuery";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
